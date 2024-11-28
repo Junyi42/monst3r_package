cef7ce012029b2f0221c44be94cad72c3f785763
junyi42 <junyi42zhang@gmail.com>
2024-11-28 19:05:51 +0000

    install raft and sam2

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
+import os
 
-print('Found packages:', find_packages())
+sam2_path = os.path.join(os.getcwd(), 'third_party', 'sam2')
+
+print('find_packages:', find_packages())
 setup(
-    description='DUSt3R as a package',
+    description='Monst3r as a package',
     name='dust3r',
-    version='0.0',    
+    version='0.0',
     packages=find_packages(),
     install_requires=[
         'torch==2.0.1',
@@ -20,6 +23,7 @@ setup(
         'tensorboard',
         'pyglet<2',
         'huggingface-hub[torch]>=0.22',
+        f"file://localhost/{sam2_path}",
     ],
     extras_require={
         'all': [
